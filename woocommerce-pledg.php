835d09c17a163a980f48a297883f4f4f99fa4e97
Lucas Fougeras <lucasfougeras@DIGITALCACTUS>
2021-02-03 10:52:48 +0100

    edit versionning

--- woocommerce-pledg.php
+++ woocommerce-pledg.php
@@ -7,7 +7,7 @@
  * Plugin URI: https://pledg.co
  * Description: Instalment payment, simple and accessible.
  * Author URI: https://fougeras.me
- * Version: 2.1.2
+ * Version: 2.1.3
  */
 
 use Firebase\JWT\JWT;
